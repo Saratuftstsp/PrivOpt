10a94b2cc21d76cc76c37197dc17dd122df8ece0
Sara Alam <sara.alam@tufts.edu>
2024-10-23 09:16:21 -0400

    code for making plan files the right input for pg explain vis

--- experiment/clean_plans.py
+++ experiment/clean_plans.py
@@ -1,18 +1,27 @@
 import os
 def main():
-    dir = "/Users/saraalam/Desktop/PrivOptCode/experiment/exec_plans/"
-    dir2 = "/Users/saraalam/Desktop/PrivOptCode/experiment/exec_plans2/"
-    for f in os.listdir(dir):
-        f1 = open(dir+f, "r")
-        s=""
-        for line in f1:
-            s+= line.strip()[2:-3] + "\n"
-
-        f1.close()
+    exp_lst = ["nullfrac", "ndist", "mcf"]
+    for exp in exp_lst:
+        dir = "/Users/saraalam/Desktop/PrivOptCode/experiment/" + exp +"/exec_plans/"
+        dir2 = "/Users/saraalam/Desktop/PrivOptCode/experiment/" + exp +"/exec_plans2/"
         
-        f2 = open(dir2+f, "w")
-        f2.write(s)
-        f2.close()
+        if not os.path.exists(dir):
+            os.makedirs(dir)  # Create the directory if it doesn't exist
+            
+        if not os.path.exists(dir2):
+            os.makedirs(dir2)  # Create the directory if it doesn't existv
+
+        for f in os.listdir(dir):
+            f1 = open(dir+f, "r")
+            s=""
+            for line in f1:
+                s+= line.strip()[2:-3] + "\n"
+
+            f1.close()
+            
+            f2 = open(dir2+f, "w")
+            f2.write(s)
+            f2.close()
         
             
         
